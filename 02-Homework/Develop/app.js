73018711908936e50cceb468776f56fbd819ce31
aaivey22 <angelawings22@gmail.com>
2020-04-21 01:29:58 -0400

    developing member intake form on app js

--- 02-Homework/Develop/app.js
+++ 02-Homework/Develop/app.js
@@ -7,9 +7,56 @@ const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
+const PORT = process.env.PORT || 7000;
 
 const render = require("./lib/htmlRenderer");
 
+const roster = [];
+
+const intake = [
+    {
+        type: "input",
+        message: "Enter the team member name",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Enter the team member role",
+        name: "role"
+    },
+    {
+        type: "input",
+        message: "Enter the team member GitHub username",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "Enter the team member email address",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Enter the team member ID number",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Enter the team member office nember",
+        name: "office"
+    }
+];
+
+function generateRoster() {
+    inquirer.prompt(intake)
+        .then(response => {
+            console.log(response.data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+};
+
+generateRoster();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -18,7 +65,7 @@ const render = require("./lib/htmlRenderer");
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
+// After you have your html, you"re now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
